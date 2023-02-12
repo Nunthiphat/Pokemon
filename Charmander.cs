b8393af340740130e5e055bada444e389bffa58d
Nunthiphat <nunthiphat.b@kkumail.com>
2023-02-12 21:47:10 +0700

    Initial commit

---
+++ Charmander.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon.pokemons
+{
+    internal class Charmander
+    {
+        private Bitmap _image = 
+            global::Pokemon.Properties.Resources.;
+        public Charmander()
+        {
+
+        }
+    }
+}
